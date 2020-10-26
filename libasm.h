3e42bc1e8092244ec5e1dcb3443d3db6b6ad5d3e
Artur <veldergard@gmail.com>
2020-10-26 16:36:33 +0300

    header

--- libasm.h
+++ libasm.h
@@ -6,18 +6,20 @@
 /*   By: olaurine <olaurine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 16:13:00 by olaurine          #+#    #+#             */
-/*   Updated: 2020/10/26 16:14:21 by olaurine         ###   ########.fr       */
+/*   Updated: 2020/10/26 16:36:26 by olaurine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBASM_H
 # define LIBASM_H
 
-int ft_read();
-int ft_write();
-int ft_strlen();
-int ft_strcpy();
-int ft_strcmp();
-int ft_strdup();
+# include <unistd.h>
+
+size_t		ft_strlen(const char *s);
+char		*ft_strcpy(char *dst, const char *src);
+int			ft_strcmp(const char *s1, const char *s2);
+ssize_t		ft_write(int fildes, const void *buf, size_t n_byte);
+ssize_t		ft_read(int fildes, void *buf, size_t n_byte);
+char		*ft_strdup(const char *s1);
 
 #endif
